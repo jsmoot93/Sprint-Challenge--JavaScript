d8bb57052b2cbce573572d2e2a99beaed1b35343
jsmoot93 <47538435+jsmoot93@users.noreply.github.com>
2019-03-01 13:22:55 -0500

    function and some of proto

--- challenges/tempCodeRunnerFile.js
+++ challenges/tempCodeRunnerFile.js
@@ -1,43 +1,35 @@
-zooAnimals = [{"animal_name":"Jackal, asiatic","population":5,"scientific_name":"Canis aureus","state":"Kentucky"},
-{"animal_name":"Screamer, southern","population":1,"scientific_name":"Chauna torquata","state":"Alabama"},
-{"animal_name":"White spoonbill","population":8,"scientific_name":"Platalea leucordia","state":"Georgia"},
-{"animal_name":"White-cheeked pintail","population":1,"scientific_name":"Anas bahamensis","state":"Oregon"},
-{"animal_name":"Black-backed jackal","population":2,"scientific_name":"Canis mesomelas","state":"Washington"},
-{"animal_name":"Brolga crane","population":9,"scientific_name":"Grus rubicundus","state":"New Mexico"},
-{"animal_name":"Common melba finch","population":5,"scientific_name":"Pytilia melba","state":"Pennsylvania"},
-{"animal_name":"Pampa gray fox","population":10,"scientific_name":"Pseudalopex gymnocercus","state":"Connecticut"},
-{"animal_name":"Hawk-eagle, crowned","population":10,"scientific_name":"Spizaetus coronatus","state":"Florida"},
-{"animal_name":"Australian pelican","population":5,"scientific_name":"Pelecanus conspicillatus","state":"West Virginia"}];
+// ==== Callbacks ====  
 
-/* Request 1: .forEach()
-
-The zoo wants to display both the scientific name and the animal name in front of the habitats.  Return an array with only the animal and scientific names in it.  The individual values in the array should look like this "Name: Jackal, asiatic, Scientific: Canis aureus."
-
+/* Step 1: Create a higher-order function that accepts a callback
+  * Create a higher-order function named consume that can take 3 parameters.
+  * The first two parameters can accept any argument
+  * The last parameter accepts a callback 
+  * In the body of the function return the callback with the two parameters that you created
 */
-const animalNames = [];
-zooAnimals.forEach(function(currentValue){
-  animalNames.push(`Name: ${currentValue.animal_name}, Scientific: ${currentValue.scientific_name}`)
-});
-console.log(animalNames);
+function consume(x, y, cb) {
+  return cb(x, y)
+};
 
-/* Request 2: .map()    
 
-The zoos need a list of all their animal's names (names only, not scientific) converted to lower case.  Create a new array named lowerCase and map over each name to convert them all to lower case.  Log the resut.
-
+/* Step 2: Create several functions to callback with consume();
+  * Create a function named add that returns the sum of two numbers
+  * Create a function named multiply that returns the product of two numbers 
+  * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
-let lowerCase = zooAnimals.map(function(currentValue){
-  return currentValue.animal_name.toLowerCase();
-});
-console.log(lowerCase);
 
-/* Request 3: .filter() 
+function add(x, y) {
+  console.log(x + y);
+};
 
-The zoos are concenred about animals with a lower population count. Find out which animals have a population less than 5.
+function multiply(x, y) {
+  console.log(x * y);
+};
 
-*/
-let largerPopulation = zooAnimals.filter(function(currentValue){
-  if (currentValue.population < 5) {
-    largerPopulation.push(`${currentValue.animal_name} has a population of ${currentValue.population}`);
-  }
-});
-console.log(largerPopulation);
+function greeting(x, y) {
+  console.log(`Hello ${x} ${y}, nice to meet you!`);
+};
+
+/* Step 3: Check your work by un-commenting the following calls to consume(): */
+ consume(2,2,add); // 4
+ consume(10,16,multiply); // 160
+ consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
